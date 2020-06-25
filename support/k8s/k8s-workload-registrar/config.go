fc9e5bb67a560ded91418cbdeea90eeb69abe67e
Faisal Memon <f.memon@f5.com>
2020-06-25 13:37:10 -0700

    Split out config for different modes

--- support/k8s/k8s-workload-registrar/config.go
+++ support/k8s/k8s-workload-registrar/config.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
 	"io/ioutil"
 
 	"github.com/hashicorp/hcl"
+	"github.com/spiffe/spire/pkg/common/log"
+	"github.com/spiffe/spire/proto/spire/api/registration"
+	"github.com/spiffe/spire/support/k8s/k8s-workload-registrar/controllers"
 	"github.com/zeebo/errs"
+	"google.golang.org/grpc"
+
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 const (
@@ -21,47 +28,184 @@ const (
 	defaultMode = modeWebhook
 )
 
-type Config struct {
+func LoadConfig(path string) (Config, error) {
+	hclBytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, errs.New("unable to load configuration: %v", err)
+	}
+
+	c := &CommonConfig{}
+	c.ParseConfig(string(hclBytes))
+
+	var config Config
+	switch c.Mode {
+	case modeCRD:
+		config = &CRDConfig{
+			CommonConfig: *c,
+		}
+	default:
+		config = &WebhookConfig{
+			CommonConfig: *c,
+		}
+	}
+
+	err = config.ParseConfig(string(hclBytes))
+	return config, err
+}
+
+type Config interface {
+	ParseConfig(hclConfig string) error
+	Run(ctx context.Context) error
+}
+
+type CommonConfig struct {
 	LogFormat                      string   `hcl:"log_format"`
 	LogLevel                       string   `hcl:"log_level"`
 	LogPath                        string   `hcl:"log_path"`
-	Addr                           string   `hcl:"addr"`
-	CertPath                       string   `hcl:"cert_path"`
-	KeyPath                        string   `hcl:"key_path"`
-	CaCertPath                     string   `hcl:"cacert_path"`
-	InsecureSkipClientVerification bool     `hcl:"insecure_skip_client_verification"`
 	TrustDomain                    string   `hcl:"trust_domain"`
 	ServerSocketPath               string   `hcl:"server_socket_path"`
 	Cluster                        string   `hcl:"cluster"`
 	PodLabel                       string   `hcl:"pod_label"`
 	PodAnnotation                  string   `hcl:"pod_annotation"`
-	DisabledNamespaces             []string `hcl:"disabled_namespaces"`
 	Mode                           string   `hcl:"mode"`
-
-	PodController bool `hcl:"pod_controller"`
-	AddSvcDNSName bool `hcl:"add_svc_dns_name"`
 }
 
-func LoadConfig(path string) (*Config, error) {
-	hclBytes, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, errs.New("unable to load configuration: %v", err)
-	}
-	return ParseConfig(string(hclBytes))
-}
-
-func ParseConfig(hclConfig string) (*Config, error) {
-	c := new(Config)
+func (c *CommonConfig) ParseConfig (hclConfig string) error {
 	c.Mode = defaultMode
-	c.PodController = defaultPodController
-	c.AddSvcDNSName = defaultAddSvcDNSName
-
 	if err := hcl.Decode(c, hclConfig); err != nil {
-		return nil, errs.New("unable to decode configuration: %v", err)
+		return errs.New("unable to decode configuration: %v", err)
 	}
-
 	if c.LogLevel == "" {
 		c.LogLevel = defaultLogLevel
+	}
+	if c.ServerSocketPath == "" {
+		return errs.New("server_socket_path must be specified")
+	}
+	if c.TrustDomain == "" {
+		return errs.New("trust_domain must be specified")
+	}
+	if c.Cluster == "" {
+		return errs.New("cluster must be specified")
+	}
+	if c.PodLabel != "" && c.PodAnnotation != "" {
+		return errs.New("workload registration mode specification is incorrect, can't specify both pod_label and pod_annotation")
+	}
+	if c.Mode != modeCRD && c.Mode != modeWebhook {
+		return errs.New("invalid mode \"%s\", valid values are %s and %s", c.Mode, modeCRD, modeWebhook)
+	}
+	return nil
+}
+
+func (c *CommonConfig) Run(ctx context.Context) error {
+	return nil
+}
+
+type CRDConfig struct {
+	CommonConfig
+	AddSvcDNSName       bool `hcl:"add_svc_dns_name"`
+	DisabledNamespaces []string `hcl:"disabled_namespaces"`
+	PodController       bool `hcl:"pod_controller"`
+}
+
+func (c *CRDConfig) ParseConfig (hclConfig string) error {
+	c.PodController = defaultPodController
+	c.AddSvcDNSName = defaultAddSvcDNSName
+	if err := hcl.Decode(c, hclConfig); err != nil {
+		return errs.New("unable to decode configuration: %v", err)
+	}
+
+	if c.DisabledNamespaces == nil {
+		c.DisabledNamespaces = defaultDisabledNamespaces()
+	}
+	return nil
+}
+
+func (c *CRDConfig) Run(ctx context.Context) error {
+	log, err := log.NewLogger(log.WithLevel(c.LogLevel), log.WithFormat(c.LogFormat), log.WithOutputFile(c.LogPath))
+	if err != nil {
+		return err
+	}
+	defer log.Close()
+
+	log.WithField("socket_path", c.ServerSocketPath).Info("Dialing server")
+	serverConn, err := grpc.DialContext(ctx, "unix://"+c.ServerSocketPath, grpc.WithInsecure())
+	if err != nil {
+		return errs.New("failed to dial server: %v", err)
+	}
+	defer serverConn.Close()
+
+	mgr, err := controllers.NewManager()
+	if err != nil {
+		return err
+	}
+
+	log.Info("Initializing SPIFFE ID CRD Mode")
+	_, err = controllers.NewSpiffeIDReconciler(controllers.SpiffeIDReconcilerConfig{
+		Log:         log,
+		Mgr:         mgr,
+		R:           registration.NewRegistrationClient(serverConn),
+		Cluster:     c.Cluster,
+		TrustDomain: c.TrustDomain,
+	})
+	if err != nil {
+		return err
+	}
+
+	if c.PodController {
+		_, err = controllers.NewNodeReconciler(controllers.NodeReconcilerConfig{
+			Log:         log,
+			Mgr:         mgr,
+			R:           registration.NewRegistrationClient(serverConn),
+			Cluster:     c.Cluster,
+			TrustDomain: c.TrustDomain,
+		})
+		if err != nil {
+			return err
+		}
+		_, err := controllers.NewPodReconciler(controllers.PodReconcilerConfig{
+			Log:                log,
+			Mgr:                mgr,
+			TrustDomain:        c.TrustDomain,
+			Cluster:            c.Cluster,
+			PodLabel:           c.PodLabel,
+			PodAnnotation:      c.PodAnnotation,
+			DisabledNamespaces: c.DisabledNamespaces,
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	if c.AddSvcDNSName {
+		_, err := controllers.NewEndpointReconciler(controllers.EndpointReconcilerConfig{
+			Log:                log,
+			Mgr:                mgr,
+			DisabledNamespaces: c.DisabledNamespaces,
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return mgr.Start(ctrl.SetupSignalHandler())
+}
+
+func defaultDisabledNamespaces() []string {
+	return []string{"kube-system"}
+}
+
+type WebhookConfig struct {
+	CommonConfig
+	Addr                           string   `hcl:"addr"`
+	CaCertPath                     string   `hcl:"cacert_path"`
+	CertPath                       string   `hcl:"cert_path"`
+	InsecureSkipClientVerification bool     `hcl:"insecure_skip_client_verification"`
+	KeyPath                        string   `hcl:"key_path"`
+}
+
+func (c *WebhookConfig) ParseConfig(hclConfig string) error {
+	if err := hcl.Decode(c, hclConfig); err != nil {
+		return errs.New("unable to decode configuration: %v", err)
 	}
 	if c.Addr == "" {
 		c.Addr = defaultAddr
@@ -75,29 +219,48 @@ func ParseConfig(hclConfig string) (*Config, error) {
 	if c.KeyPath == "" {
 		c.KeyPath = defaultKeyPath
 	}
-	if c.ServerSocketPath == "" {
-		return nil, errs.New("server_socket_path must be specified")
-	}
-	if c.TrustDomain == "" {
-		return nil, errs.New("trust_domain must be specified")
-	}
-	if c.Cluster == "" {
-		return nil, errs.New("cluster must be specified")
-	}
-	if c.PodLabel != "" && c.PodAnnotation != "" {
-		return nil, errs.New("workload registration mode specification is incorrect, can't specify both pod_label and pod_annotation")
-	}
-	if c.DisabledNamespaces == nil {
-		c.DisabledNamespaces = defaultDisabledNamespaces()
-	}
-
-	if c.Mode != modeCRD && c.Mode != modeWebhook {
-		return nil, errs.New("invalid mode \"%s\", valid values are %s and %s", c.Mode, modeCRD, modeWebhook)
-	}
-
-	return c, nil
+	return nil
 }
 
-func defaultDisabledNamespaces() []string {
-	return []string{"kube-system"}
+func (c *WebhookConfig) Run(ctx context.Context) error {
+	log, err := log.NewLogger(log.WithLevel(c.LogLevel), log.WithFormat(c.LogFormat), log.WithOutputFile(c.LogPath))
+	if err != nil {
+		return err
+	}
+	defer log.Close()
+
+	log.WithField("socket_path", c.ServerSocketPath).Info("Dialing server")
+	serverConn, err := grpc.DialContext(ctx, "unix://"+c.ServerSocketPath, grpc.WithInsecure())
+	if err != nil {
+		return errs.New("failed to dial server: %v", err)
+	}
+	defer serverConn.Close()
+
+	controller := NewController(ControllerConfig{
+		Log:           log,
+		R:             registration.NewRegistrationClient(serverConn),
+		TrustDomain:   c.TrustDomain,
+		Cluster:       c.Cluster,
+		PodLabel:      c.PodLabel,
+		PodAnnotation: c.PodAnnotation,
+	})
+
+	log.Info("Initializing registrar")
+	if err := controller.Initialize(ctx); err != nil {
+		return err
+	}
+
+	server, err := NewServer(ServerConfig{
+		Log:                            log,
+		Addr:                           c.Addr,
+		Handler:                        NewWebhookHandler(controller),
+		CertPath:                       c.CertPath,
+		KeyPath:                        c.KeyPath,
+		CaCertPath:                     c.CaCertPath,
+		InsecureSkipClientVerification: c.InsecureSkipClientVerification,
+	})
+	if err != nil {
+		return err
+	}
+	return server.Run(ctx)
 }
--- support/k8s/k8s-workload-registrar/main.go
+++ support/k8s/k8s-workload-registrar/main.go
@@ -5,14 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-
-	"github.com/spiffe/spire/pkg/common/log"
-	"github.com/spiffe/spire/proto/spire/api/registration"
-	"github.com/spiffe/spire/support/k8s/k8s-workload-registrar/controllers"
-	"github.com/zeebo/errs"
-	"google.golang.org/grpc"
-
-	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 var (
@@ -33,106 +25,5 @@ func run(ctx context.Context, configPath string) error {
 		return err
 	}
 
-	log, err := log.NewLogger(log.WithLevel(config.LogLevel), log.WithFormat(config.LogFormat), log.WithOutputFile(config.LogPath))
-	if err != nil {
-		return err
-	}
-	defer log.Close()
-
-	log.WithField("socket_path", config.ServerSocketPath).Info("Dialing server")
-	serverConn, err := grpc.DialContext(ctx, "unix://"+config.ServerSocketPath, grpc.WithInsecure())
-	if err != nil {
-		return errs.New("failed to dial server: %v", err)
-	}
-	defer serverConn.Close()
-
-	switch config.Mode {
-	case modeWebhook:
-		controller := NewController(ControllerConfig{
-			Log:           log,
-			R:             registration.NewRegistrationClient(serverConn),
-			TrustDomain:   config.TrustDomain,
-			Cluster:       config.Cluster,
-			PodLabel:      config.PodLabel,
-			PodAnnotation: config.PodAnnotation,
-		})
-
-		log.Info("Initializing registrar")
-		if err := controller.Initialize(ctx); err != nil {
-			return err
-		}
-
-		server, err := NewServer(ServerConfig{
-			Log:                            log,
-			Addr:                           config.Addr,
-			Handler:                        NewWebhookHandler(controller),
-			CertPath:                       config.CertPath,
-			KeyPath:                        config.KeyPath,
-			CaCertPath:                     config.CaCertPath,
-			InsecureSkipClientVerification: config.InsecureSkipClientVerification,
-		})
-		if err != nil {
-			return err
-		}
-		return server.Run(ctx)
-	case modeCRD:
-		log.Info("Initializing SPIFFE ID CRD Mode")
-		mgr, err := controllers.NewManager()
-		if err != nil {
-			return err
-		}
-
-		_, err = controllers.NewSpiffeIDReconciler(controllers.SpiffeIDReconcilerConfig{
-			Log:         log,
-			Mgr:         mgr,
-			R:           registration.NewRegistrationClient(serverConn),
-			Cluster:     config.Cluster,
-			TrustDomain: config.TrustDomain,
-		})
-		if err != nil {
-			return err
-		}
-
-		if config.PodController {
-			_, err = controllers.NewNodeReconciler(controllers.NodeReconcilerConfig{
-				Log:         log,
-				Mgr:         mgr,
-				R:           registration.NewRegistrationClient(serverConn),
-				Cluster:     config.Cluster,
-				TrustDomain: config.TrustDomain,
-			})
-			if err != nil {
-				return err
-			}
-			_, err := controllers.NewPodReconciler(controllers.PodReconcilerConfig{
-				Log:                log,
-				Mgr:                mgr,
-				TrustDomain:        config.TrustDomain,
-				Cluster:            config.Cluster,
-				PodLabel:           config.PodLabel,
-				PodAnnotation:      config.PodAnnotation,
-				DisabledNamespaces: config.DisabledNamespaces,
-			})
-			if err != nil {
-				return err
-			}
-		}
-
-		if config.AddSvcDNSName {
-			_, err := controllers.NewEndpointReconciler(controllers.EndpointReconcilerConfig{
-				Log:                log,
-				Mgr:                mgr,
-				DisabledNamespaces: config.DisabledNamespaces,
-			})
-			if err != nil {
-				return err
-			}
-		}
-
-		if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-			return err
-		}
-	}
-
-	return nil
+	return config.Run(ctx)
 }
