281afb31f5642e0cba1dd0ca99409d624fe03796
Faisal Memon <fymemon@yahoo.com>
2025-03-21 18:16:15 -0700

    Fetch updated cache entries in bulk
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>

--- pkg/server/endpoints/authorized_entryfetcher_registration_entries.go
+++ pkg/server/endpoints/authorized_entryfetcher_registration_entries.go
@@ -218,12 +218,21 @@ func (a *registrationEntries) updateCache(ctx context.Context) error {
 
 // updateCacheEntry update/deletes/creates an individual registration entry in the cache.
 func (a *registrationEntries) updateCachedEntries(ctx context.Context) error {
+	entryIds := make([]string, 0, len(a.fetchEntries))
+	fmt.Println(len(a.fetchEntries))
 	for entryId := range a.fetchEntries {
-		commonEntry, err := a.ds.FetchRegistrationEntry(ctx, entryId)
-		if err != nil {
-			continue
-		}
+		entryIds = append(entryIds, entryId)
+	}
 
+	commonEntries, err := a.ds.FetchRegistrationEntries(ctx, entryIds)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(commonEntries)
+
+	for entryId := range a.fetchEntries {
+		commonEntry := commonEntries[entryId]
 		if commonEntry == nil {
 			a.cache.RemoveEntry(entryId)
 			delete(a.fetchEntries, entryId)
@@ -238,9 +247,11 @@ func (a *registrationEntries) updateCachedEntries(ctx context.Context) error {
 			continue
 		}
 
+		fmt.Printf("Updated entry: %s", entryId)
 		a.cache.UpdateEntry(entry)
 		delete(a.fetchEntries, entryId)
 	}
+
 	return nil
 }
 
